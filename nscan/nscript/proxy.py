a0f5fd0f22c411ab1c9da6098480544d2a3220b9
OffensivePython <pyt3sting@gmail.com>
2015-02-05 04:45:21 +0100

    Fixing SOCKS5 check

--- nscan/nscript/proxy.py
+++ nscan/nscript/proxy.py
@@ -8,7 +8,7 @@ import logging
 
 def SendRecv(ip, port, packet):
 	s = socket.socket()
-	s.settimeout(3)
+	s.settimeout(5)
 	try:
 		s.connect((ip, port))
 		s.send(packet)
@@ -19,23 +19,25 @@ def SendRecv(ip, port, packet):
 	return data
 
 def SOCKS(ip, port):
-	version = ''
-	CD = '\x01' # Command: CONNECT
-	DSTPORT = '\x00\x50' # Destination Port: 80
-	try:
-		DSTIP = socket.inet_aton(socket.gethostbyname('www.google.com'))
-	except socket.gaierror:
+    TARGET = 'www.google.com'
+    version = ''
+    CD = '\x01' # Command: CONNECT
+    DSTPORT = '\x00\x50' # Destination Port: 80
+    try:
+		DSTIP = socket.inet_aton(socket.gethostbyname(TARGET))
+    except socket.gaierror:
 		return version
-	USERID = 'Nscan\x00'
-	socks4 = struct.pack('1s1s2s4s6s', '\x04', CD, DSTPORT, DSTIP, USERID)
-	socks5 = struct.pack('1s1s2s4s6s', '\x05', CD, DSTPORT, DSTIP, USERID)
-	s4 = SendRecv(ip, port, socks4)
-	s5 = SendRecv(ip, port, socks5)
-	if s4 and s4[0]=='\x00' and s4[1]=='\x5a':
+    USERID = 'Nscan\x00'
+    socks4 = struct.pack('1s1s2s4s6s', '\x04', CD, DSTPORT, DSTIP, USERID)
+    socks5 = struct.pack('3s1s1s1s1s', '\x05\x01\x00','\x05', CD, '\x00', '\x03')
+    socks5+= hex(len(TARGET)) + TARGET + DSTPORT
+    s4 = SendRecv(ip, port, socks4)
+    s5 = SendRecv(ip, port, socks5)
+    if s4 and s4[0]=='\x00' and s4[1]=='\x5a':
 		version = '4'
-	if s5 and s5[0]=='\x00' and s5[1]=='\x5a':
+    if s5 and s5[0]=='\x05' and s5[1]=='\x00':
 		version+='/5'
-	return version
+    return version
 
 def run(queue, event):
 	while True:
