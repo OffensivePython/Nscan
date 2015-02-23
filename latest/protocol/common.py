8b56a0fd59debc50d3e177c548887a30e18a66af
OffensivePython <pyt3sting@gmail.com>
2015-02-23 09:40:01 +0100

    Update and rename latest to latest/protocol/common.py

---
+++ latest/protocol/common.py
@@ -0,0 +1,11 @@
+def checksum(data):
+    s = 0
+    n = len(data) % 2
+    for i in range(0, len(data)-n, 2):
+        s+= ord(data[i]) + (ord(data[i+1]) << 8)
+    if n:
+        s+= ord(data[i+1])
+    while (s >> 16):
+        s = (s & 0xFFFF) + (s >> 16)
+    s = ~s & 0xffff
+    return s
